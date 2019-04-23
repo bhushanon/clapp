c71043e09508745877e03b895d31e28aa35c7404
Bhushan Dongare <M1020071@mindtree.com>
2019-04-23 14:58:23 +0530

    Fixing Incorrect Files

---
+++ webpack/src/main/java/com/myapps/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.myapps.config.audit;
---
+++ webpack/src/main/java/com/myapps/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.myapps.repository;
+
+import com.myapps.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
